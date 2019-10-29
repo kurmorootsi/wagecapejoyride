fbedaa64c176dab9f14a3461683e1ac18dbbd9e9
kurmorootsi <kurmo.rootsi@hotmail.com>
2019-10-29 18:46:00 +0200

    first commit

---
+++ Wage Cap Joyride/Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public GameObject prevCeiling;
+    public GameObject ceiling;
+    public GameObject floor;
+    public GameObject prevFloor;
+
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player.transform.position.x > floor.transform.position.x)
+        {
+            var tempCeiling = prevCeiling;
+            var tempFloor = prevFloor;
+            prevCeiling = ceiling;
+            prevFloor = floor;
+            tempCeiling.transform.position += new Vector3(80, 0, 0);
+            tempFloor.transform.position += new Vector3(80, 0, 0);
+            ceiling = tempCeiling;
+            floor = tempFloor;
+        }
+    }
+}
---
+++ Wage Cap Joyride/Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    Rigidbody body;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            body.AddForce(new Vector3(0, 50, 0), ForceMode.Acceleration);
+        } else if (Input.GetMouseButtonUp(0)) {
+            body.velocity *= 0.25f;
+        }
+    }
+}
