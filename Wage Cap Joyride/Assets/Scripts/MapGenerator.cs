98fe64f1577b3cb5eb8e095d8875a994af5876d8
Elinor Roosalu <elinorroosalu@users.noreply.github.com>
2019-11-01 20:39:00 +0200

    Obstacles added

--- Wage Cap Joyride/Assets/Scripts/MapGenerator.cs
+++ Wage Cap Joyride/Assets/Scripts/MapGenerator.cs
@@ -11,10 +11,37 @@ public class MapGenerator : MonoBehaviour
 
     public GameObject player;
 
+    public GameObject obstacle1;
+    public GameObject obstacle2;
+    public GameObject obstacle3;
+    public GameObject obstacle4;
+
+    public GameObject obstaclePrefab;
+
+    public float minObstacleY;
+    public float maxObstacleY;
+
+    public float minObstacleSpacing;
+    public float maxObstacleSpacing;
+
+    public float minObstacleScaleY;
+    public float maxObstacleScaleX;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        obstacle1 = GenerateObstacle(player.transform.position.x + 10);
+        obstacle2 = GenerateObstacle(obstacle1.transform.position.x);
+        obstacle3 = GenerateObstacle(obstacle2.transform.position.x);
+        obstacle4 = GenerateObstacle(obstacle3.transform.position.x);
+
+        GameObject GenerateObstacle(float referenceX){
+        	GameObject obstacle = GameObject.Instantiate(
+        	obstaclePrefab);
+        obstacle.transform.position = new Vector3(referenceX + 10f + 
+        	Random.Range(minObstacleSpacing, maxObstacleSpacing), Random.Range(minObstacleY, maxObstacleY),0);
+        	return obstacle;
+        }
     }
 
     // Update is called once per frame
