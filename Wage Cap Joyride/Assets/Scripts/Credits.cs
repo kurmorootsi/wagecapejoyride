0aef9bc331e1cfa43eb34ae9b31d3cb2ceaaa393
Elinor Roosalu <elinorroosalu@users.noreply.github.com>
2019-12-23 20:58:47 +0200

    Credits

---
+++ Wage Cap Joyride/Assets/Scripts/Credits.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HowToPlay : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 4);
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 3);
+    }
+}
